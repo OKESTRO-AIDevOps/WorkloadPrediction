42b9635d24b46676ad9830de2ebaed6a4d977b85
okestro-ynlee <yn.lee@okestro.com>
2022-12-20 16:21:20 +0900

    workloadpediction commit

--- pipelines/prediction/prediction_pipeline.py
+++ pipelines/prediction/prediction_pipeline.py
@@ -22,3 +22,28 @@ prediction_component = components.create_component_from_func(
         base_image='path/your/image'
     )
 
+
+
+@dsl.pipeline(
+    name="croffle-prediction_pvc",
+    description = "croffle prediction pipeline using pvc"
+)
+def prediction_pipeline(cpu_request :str="4000m", 
+                        cpu_limit : str="8000m",
+                        memory_request : str="4000Mi",
+                        memory_limit : str="16000Mi"):
+    # base image
+    dsl.get_pipeline_conf().set_image_pull_secrets([kubernetes.client.V1LocalObjectReference(name="public_aipops")])
+    vop = dsl.PipelineVolume(pvc='croffle-pvc')    
+
+    # pvc mount path
+    mount_path = "/aiplatform/"
+ 
+    # task 
+    prdiction_task = prediction_component().set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})
+
+    dsl.get_pipeline_conf().set_ttl_seconds_after_finished(20)
