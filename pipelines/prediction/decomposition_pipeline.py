2f0d28580e968488ae8709d700d873e98d9a01a5
okestro-jelee <je.lee@okestro.com>
2022-12-21 10:44:27 +0900

    workloadprediction commit

--- pipelines/prediction/decomposition_pipeline.py
+++ pipelines/prediction/decomposition_pipeline.py
@@ -11,7 +11,7 @@ from kubernetes.client.models.v1_affinity import V1Affinity
 from kubernetes.client.models.v1_node_affinity import V1NodeAffinity
 from kubernetes.client.models.v1_node_selector_requirement import V1NodeSelectorRequirement
 
-client = kfp.Client(host='http://34.64.153.235:30020')
+client = kfp.Client(host='IP address')
 
 
 
@@ -31,3 +31,34 @@ decomposition_component = components.create_component_from_func(
         func=decomposition,
         base_image='okestroaiops/prediction:latest'
     )
+
+
+
+@dsl.pipeline(
+    name="croffle-decomposition",
+    description = "croffle decompositio pipeline"
+)
+def decomposition_pipeline(cpu_request :str="4000m", 
+                        cpu_limit : str="8000m",
+                        memory_request : str="4000Mi",
+                        memory_limit : str="16000Mi",
+                        host_thr : int=60
+                        ):
+    # base image
+    dsl.get_pipeline_conf().set_image_pull_secrets([kubernetes.client.V1LocalObjectReference(name="okestroaiops")])
+    vop = dsl.PipelineVolume(pvc='croffle-pvc')    
+    mount_path = '/symphony/'
+ 
+
+    decomposition_vm_disk_write = decomposition_component('vm', 'diskio-write', host_thr).set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})
+                                            
+    decomposition_vm_disk_read = decomposition_component('vm', 'diskio-read', host_thr).set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})\
+                                            .after(decomposition_vm_disk_write)
