0561892c2c5b0df3b6e23a4d14cca7176c8370c6
okestro-ynlee <yn.lee@okestro.com>
2022-12-21 10:42:49 +0900

    workloadprediction commit

--- pipelines/prediction/prediction_pipeline.py
+++ pipelines/prediction/prediction_pipeline.py
@@ -32,14 +32,11 @@ def prediction_pipeline(cpu_request :str="4000m",
                         cpu_limit : str="8000m",
                         memory_request : str="4000Mi",
                         memory_limit : str="16000Mi"):
-    # base image
     dsl.get_pipeline_conf().set_image_pull_secrets([kubernetes.client.V1LocalObjectReference(name="public_aipops")])
     vop = dsl.PipelineVolume(pvc='croffle-pvc')    
 
-    # pvc mount path
     mount_path = "/aiplatform/"
- 
-    # task 
+
     prdiction_task = prediction_component().set_cpu_limit(cpu_limit)\
                                             .set_memory_limit(memory_limit)\
                                             .set_cpu_request(cpu_request)\
@@ -47,3 +44,29 @@ def prediction_pipeline(cpu_request :str="4000m",
                                             .add_pvolumes({mount_path: vop})
 
     dsl.get_pipeline_conf().set_ttl_seconds_after_finished(20)
+
+
+
+client.create_run_from_pipeline_func(prediction_pipeline, arguments={})
+    
+
+kfp.compiler.Compiler().compile(
+    pipeline_func=prediction_pipeline,
+    package_path='prediction_pl.yaml'
+)
+
+client.create_recurring_run(
+    experiment_id = client.get_experiment(experiment_name="Default").id,
+    job_name="prediction_croffle pvc",
+    description="version: croffle:prediction_pvc",
+    cron_expression="0 0 17 * * *",
+    pipeline_package_path = "prediction_pl.yaml",
+)
+
+
+
+client.upload_pipeline(
+    pipeline_package_path='prediction_pl.yaml',
+    pipeline_name = "prediction_croffle_pvc",
+    description = "version: croffle:prediction_pvc"
+)
